074d50f439f3ec8d79d75b276ff2e002087f27da
unknown <martin@joyouscoding.com>
2023-07-20 10:48:35 +0200

    Azote

---
+++ src/components/links.js
@@ -0,0 +1,96 @@
+const NAVIGATION = [
+  {
+    title: 'Présentation',
+    links: [
+      { title: 'A propos', href: '/', status: 'info' },
+      { title: 'Participer', href: '/infos/participer', status: 'join' },
+    ],
+  },
+  {
+    title: 'Sols Vivants',
+    links: [
+      {
+        title: 'Bilan humique',
+        href: '/docs/solsvivants/bilanhumique',
+        status: 'done',
+      },
+      {
+        title: 'Couverture du sol',
+        href: '/docs/solsvivants/couverture',
+        status: 'done',
+      },
+      {
+        title: 'Simplification du travail du sol',
+        href: '/docs/solsvivants/simplification',
+        status: 'done',
+      },
+      {
+        title: 'Diversité de cultures',
+        href: '/docs/solsvivants/diversite',
+        status: 'done',
+      },
+    ],
+  },
+  {
+    title: 'Biodiversité et Eau',
+    links: [
+      {
+        title: "Progression vers l'AB",
+        href: '/docs/biodiversite/progressionab',
+        status: 'done',
+      },
+      { title: 'Mailage écologique', href: '/docs/wip?4' },
+      {
+        title: 'Fragmentation spatiale',
+        href: '/docs/biodiversite/fragmentation-spatiale',
+        status: 'done',
+      },
+      { title: "Résistance au lessivage de l'azote", href: '/docs/wip?6' },
+    ],
+  },
+  {
+    title: 'Autonomie et Résilience',
+    links: [
+      {
+        title: "Autonomie alimentaire de l'élevage",
+        href: '/docs/autonomieresilience/autonomie-alimentaire',
+        status: 'done',
+      },
+      {
+        title: 'Cadre de vie',
+        href: '/docs/autonomieresilience/cadre-de-vie',
+        status: 'done',
+      },
+      {
+        title: 'Autonomie en azote',
+        href: '/docs/autonomieresilience/autonomie-azote',
+        status: 'done',
+      },
+      { title: 'Indépendance aux primes', href: '/docs/wip?9' },
+      { title: 'Indépendance énergétique', href: '/docs/wip?10' },
+    ],
+  },
+  {
+    title: 'Rentabilité et Efficience',
+    links: [
+      {
+        title: 'Efficience des intrants',
+        href: '/docs/solsvivants/efficience-intrants',
+        status: 'wip',
+      },
+      { title: 'Rendement SAU', href: '/docs/wip?12' },
+      {
+        title: 'Marge brute moyenne des cultures',
+        href: '/docs/rentabilite/marge-brute',
+        status: 'done',
+      },
+      {
+        title: 'Performance Alimentaire',
+        href: '/docs/rentabilite/perfalim',
+        status: 'done',
+      },
+    ],
+  },
+]
+
+export default NAVIGATION
---
+++ src/pages/docs/autonomieresilience/autonomie-azote.md
@@ -0,0 +1,40 @@
+---
+title: Autonomie en Azote
+description: Autonomie en Azote.
+---
+
+## Description
+
+Nous utilisons cet indicateur pour évaluer l’autonomie de la ferme sur sa fertilisation. Il permet de savoir quel pourcentage de l’azote apporté provient de la ferme ou des alentours. Le but est également d’apporter une dimension territoriale en favorisant la coopération entre les élevages dans une même région afin de créer une synergie entre les voisins.
+
+---
+
+## Unité
+
+Kilos d'Azote par hectare
+
+## Comment est-ce calculé ?
+
+C’est le pourcentage de la quantité d’azote fixée par le système via les légumineuses, recyclé par le couvert végétaux et apporté par la fertilisation organique locale (avec un barème décroissant en fonction de la distance d’approvisionnement), par rapport à la quantité d’azote totale apportée à la plante:
+
+Barème décroissant :
+
+- 1 à 20km = local
+- De 21 à 120km = diminution progressive de 1% par kilomètre
+
+Le calcul consiste à diviser la quantité d’azote locale (fertilisation apportée par les couverts et les légumineuses et fertilisation organique multipliée par le coefficient du barème) par la quantité d’azote totale (fertilisation apportée par les couverts et les légumineuses, fertilisation organique et fertilisation minérale)
+
+{% math expression="Autonomie\\,en\\,N = \\frac {\\sum Azote\\,Local} {\\sum Azote\\,Total}" p="6" /%}
+
+---
+
+## D’où proviennent les données ?
+
+Les données sont renseigné par l'agriculteur et encodée par l'agronome.
+
+---
+
+## {% compass quarter="nw" inline="true" /%} Dans la boussole
+
+- Min : 0% pour une ferme qui n’utiliserait qu’une fertilisation exogène lointaine
+- Max : 100% autonome complètement grâce aux couverts, aux légumineuses et à ses interactions avec l’élevage pour une fertilisation de proximité.
