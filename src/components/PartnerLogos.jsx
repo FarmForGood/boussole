f0c95fb6481815b60dd446ca0f76a59f24810fd8
unknown <martin@joyouscoding.com>
2023-09-26 12:08:10 +0200

    Names with logos

--- src/components/PartnerLogos.jsx
+++ src/components/PartnerLogos.jsx
@@ -1,90 +1,85 @@
 export default function PartnerLogos() {
   let partners = [
-    [
-      {
-        name: 'Bister',
-        logo: '/logos/bister.jpg',
-        url: 'https://bister.be/',
-      },
-      {
-        name: 'Puratos',
-        logo: '/logos/puratos.svg',
-        url: 'https://www.puratos.com/',
-      },
-      {
-        name: 'Dandoy',
-        logo: '/logos/dandoy.jpg',
-        url: 'https://maisondandoy.com/',
-      },
-    ],
-    [
-      {
-        name: "Copains d'abord",
-        logo: '/logos/copains.svg',
-        url: 'https://copains.group/',
-      },
-      {
-        name: 'Farm For Good!',
-        logo: '/logos/farmforgood.svg',
-        url: 'https://farmforgood.org/',
-      },
-      {
-        name: 'Natagora',
-        logo: '/logos/natagora.png',
-        url: 'https://www.natagora.be/',
-      },
-    ],
-    [
-      {
-        name: 'Gembloux Agro-Bio Tech',
-        logo: '/logos/gembloux.svg',
-        url: 'https://www.gembloux.uliege.be/cms/c_4039827/en/gembloux-agro-bio-tech/',
-      },
-      {
-        name: 'Centre Michamps',
-        logo: '/logos/michamps.png',
-        url: 'https://centredemichamps.be/',
-      },
-      {
-        name: "Parc naturel des Plaines de l'Escaut",
-        logo: '/logos/escaut.png',
-        url: 'https://plainesdelescaut.be/wikipnpe/?PagePrincipale/',
-      },
-    ],
-    [
-      {
-        name: 'Collège des Producteurs',
-        logo: '/logos/college_producteurs.jpg',
-        url: 'https://collegedesproducteurs.be',
-      },
-      {
-        name: 'BioWallonie',
-        logo: '/logos/biowallonie.png',
-        url: 'https://www.biowallonie.com',
-      },
-      {
-        name: 'Unab',
-        logo: '/logos/unab.png',
-        url: 'https://www.unab-bio.be/',
-      },
-    ],
-    [
-      {
-        name: 'Fibl Suisse',
-        logo: '/logos/FiBL.png',
-        url: 'https://www.fibl.org/fr/sites/suisse/',
-      },
-      {
-        name: 'Foire de Libramont',
-        logo: '/logos/libramont.jpg',
-        url: 'https://www.foiredelibramont.com/',
-      },
-      {
-        name: "Graines d'Avenir",
-        logo: '/logos/graines_avenir.jpg',
-        url: 'https://www.facebook.com/FormationsAgroecologie/',
-      },
-    ],
+    {
+      name: 'Bister',
+      logo: '/logos/bister.jpg',
+      url: 'https://bister.be/',
+    },
+    {
+      name: 'Puratos',
+      logo: '/logos/puratos.svg',
+      url: 'https://www.puratos.com/',
+    },
+    {
+      name: 'Dandoy',
+      logo: '/logos/dandoy.jpg',
+      url: 'https://maisondandoy.com/',
+    },
+    {
+      name: "Copains d'abord",
+      logo: '/logos/copains.svg',
+      url: 'https://copains.group/',
+    },
+    {
+      name: 'Farm For Good!',
+      logo: '/logos/farmforgood.svg',
+      url: 'https://farmforgood.org/',
+    },
+    {
+      name: 'Natagora',
+      logo: '/logos/natagora.png',
+      url: 'https://www.natagora.be/',
+    },
+    {
+      name: 'Gembloux Agro-Bio Tech',
+      logo: '/logos/gembloux.svg',
+      url: 'https://www.gembloux.uliege.be/cms/c_4039827/en/gembloux-agro-bio-tech/',
+    },
+    {
+      name: 'Centre Michamps',
+      logo: '/logos/michamps.png',
+      url: 'https://centredemichamps.be/',
+    },
+    {
+      name: "Parc naturel des Plaines de l'Escaut",
+      logo: '/logos/escaut.png',
+      url: 'https://plainesdelescaut.be/wikipnpe/?PagePrincipale/',
+    },
+    {
+      name: 'Collège des Producteurs',
+      logo: '/logos/college_producteurs.jpg',
+      url: 'https://collegedesproducteurs.be',
+    },
+    {
+      name: 'BioWallonie',
+      logo: '/logos/biowallonie.png',
+      url: 'https://www.biowallonie.com',
+    },
+    {
+      name: 'Unab',
+      logo: '/logos/unab.png',
+      url: 'https://www.unab-bio.be/',
+    },
+    {
+      name: 'Fibl Suisse',
+      logo: '/logos/FiBL.png',
+      url: 'https://www.fibl.org/fr/sites/suisse/',
+    },
+    {
+      name: 'Foire de Libramont',
+      logo: '/logos/libramont.jpg',
+      url: 'https://www.foiredelibramont.com/',
+    },
+    {
+      name: "Graines d'Avenir",
+      logo: '/logos/graines_avenir.jpg',
+      url: 'https://www.facebook.com/FormationsAgroecologie/',
+    },
+    {
+      name: 'Certisys',
+      logo: '/logos/certisys.png',
+      url: 'https://www.certisys.eu/',
+    },
   ]
 
   function renderLogo(partner) {
@@ -94,26 +89,17 @@ export default function PartnerLogos() {
         target="_blank"
         style={{ boxShadow: 'none', textShadow: 'none' }}
         rel="noreferrer"
-        className="shadow-none"
+        className="flex flex-col items-center justify-start rounded-sm shadow-sm"
       >
-        <img src={partner.logo} alt="partner" className="m-4 h-32 w-32" />
+        <img src={partner.logo} alt="partner" className="mx-2 mt-2 h-32 w-32" />
+        <p className="text-center text-sm capitalize">{partner.name}</p>
       </a>
     )
   }
 
-  function renderLogoRow(logos) {
-    return (
-      <div className="flex flex-row justify-center" key={logos[0].url}>
-        {renderLogo(logos[0])}
-        {logos[1] && renderLogo(logos[1])}
-        {logos[2] && renderLogo(logos[2])}
-      </div>
-    )
-  }
-
   return (
-    <div className="flex flex-col">
-      {partners.map((partner) => renderLogoRow(partner))}
+    <div className="grid grid-cols-3">
+      {partners.map((partner) => renderLogo(partner))}
     </div>
   )
 }
